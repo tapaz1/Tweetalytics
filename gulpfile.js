ffc161fca70c62eb8392858ca6e13addbce4c46d
Adam Paz <timothy.paz@us.sogeti.com>
2017-02-08 15:10:52 -0600

    Adding server.js and functions.js needed for the server to start. Also added two gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,14 @@ var gulp = require('gulp');
 
 gulp.task('default', function() {
   console.log('Running gulp tasks..');
+
+});
+
+gulp.task('copy', function () {
+  console.log("Running gulp task 'copyServer'.");
+  gulp.src('src/app/server/**/*.js')
+    .pipe(gulp.dest('dist'));
+
+  gulp.src('data/**')
+    .pipe(gulp.dest('dist'));
 });
