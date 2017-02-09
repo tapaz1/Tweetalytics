158bf101a5679e2c838046c93962583ee3828c38
Adam Paz <timothy.paz@us.sogeti.com>
2017-02-09 10:58:38 -0600

    Changing front end service endpoints to see if that works..

--- src/app/services/twitterSearch.service.ts
+++ src/app/services/twitterSearch.service.ts
@@ -31,7 +31,7 @@ export class TwitterService {
     let headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
 
-    return this.http.post('/api/authorize', {headers: headers})
+    return this.http.post(process.env.HTTP_HOST + '/api/authorize', {headers: headers})
       .map((res) => {
       this.setBearerToken(res.json().data);
     });
@@ -42,7 +42,7 @@ export class TwitterService {
     headers.append('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
     let finalQuery = 'query=' + query;
 
-    return this.http.post('/api/search', finalQuery, {headers: headers})
+    return this.http.post(process.env.HTTP_HOST + '/api/search', finalQuery, {headers: headers})
   }
 
   public getBearerToken() {
