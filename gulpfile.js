9661c34a7ee6c2de640f84bf6326c7aba3aeb305
Adam Paz <timothy.paz@us.sogeti.com>
2017-02-23 10:16:05 -0600

    Adding the node exec child process command to manually run terminal commands to start server.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
+var exec = require('child_process').exec;
 
 gulp.task('default', function() {
   console.log('Running gulp tasks..');
+  exec('node server.js', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
 });
 
 gulp.task('copy', function () {
-  console.log("Running gulp task 'copyServer'.");
+  console.log("Running gulp task 'copy'.");
   gulp.src('src/app/server/**/*.js')
     .pipe(gulp.dest('./'));
 
